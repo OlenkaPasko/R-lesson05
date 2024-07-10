5446a9e39349160a5564088a2a626b359473f317
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-10 08:57:55 +0300

    Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,17 @@
 import css from "./App.module.css";
-
+import { Link, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <div className={css.container}>
       <h1>Routing in React</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/profile">Prifile</Link>
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/profile" element={<div>Profile page</div>} />
+      </Routes>
     </div>
   );
 }
