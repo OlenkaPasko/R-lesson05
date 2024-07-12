6fd32ed297f5403aebccec27afef81cb68aaba79
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-12 07:54:53 +0300

    OwnerFilter

--- src/pages/PaymentsPage.jsx
+++ src/pages/PaymentsPage.jsx
@@ -1,23 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PaymentList from "../components/PaymentList/PaymentList";
-import { getPayments } from "../payments-api"; 
+import OwnerFilter from "../components/OwnerFilter/OwnerFilter";
+import { getPayments } from "../payments-api";
+import { useSearchParams } from "react-router-dom";
+
 //стан на лодінг і ерор теж обов'язково доробити
 export default function PaymentsPage() {
-  const [payments, setPayments] = useState([]);
-   useEffect(() => {
-     async function fetchPayments() {
-       try {
-         const data = await getPayments();
-         setPayments(data);
-       } catch (error) {}
-     }
-     fetchPayments();
-   }, []);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const ownerFilter = searchParams.get("owner") ?? "";//таким чином отримуємо поточне значення фільтру
 
+  const [payments, setPayments] = useState([]);
+  //const [ownerFilter, setOwnerFilter] = useState("");
+
+  useEffect(() => {
+    async function fetchPayments() {
+      try {
+        const data = await getPayments();
+        setPayments(data);
+      } catch (error) {}
+    }
+    fetchPayments();
+  }, []);
+
+ // const filteredPayments = useMemo(() => {
+   // return payments.filter((payment) =>
+    //  payment.cardOwner.toLowerCase().includes(ownerFilter.toLowerCase())
+   // );
+  //}, [ownerFilter, payments]);
 
   return (
     <div>
-      <PaymentList payments={payments} />
+      <OwnerFilter value={ownerFilter} />
+      {payments.length > 0 && <PaymentList payments={payments} />}
     </div>
   );
 }
